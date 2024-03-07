5419a834ed40ece7e5101265a5e7899dfe52fef2
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-07 14:15:20 +0100

    added dotenv package and morgan. small changes and backend fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import Router from './route/routes.js'
+import morgan from 'morgan'
+import 'dotenv/config'
+
+const PORT = process.env.PORT || 4000
 
 const app = express()
+app.use(morgan('dev'))
 app.use(express.json()) // <= never forget this when trying to get data from reg.body or more
 app.use(Router)
 
-app.listen(4000, () => {
-  console.log('Server is running on port 3000')
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
