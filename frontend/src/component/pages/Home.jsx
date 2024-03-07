8763992df1aa741d7cb267169bebfc1048ad0122
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-07 15:57:17 +0100

    added proxy

---
+++ frontend/src/component/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <ul>
+              <li>
+                <p>Ameer</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
