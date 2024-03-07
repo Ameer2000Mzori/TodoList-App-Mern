bfa3658dff229b537e3248b746d80e8c21037f63
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-07 11:01:19 +0100

    add express and created small backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,11 @@
-console.log('hallo world!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World from backend')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
